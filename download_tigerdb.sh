1291c74943ab6a753a267f95a98e70218b8adfbd
Quanhu Sheng <shengqh@gmail.com>
2018-02-02 16:55:53 -0600

    Update download_tigerdb.sh

--- download_tigerdb.sh
+++ download_tigerdb.sh
@@ -1,7 +1,4 @@
 
-mkdir bowtie_index
-cd bowtie_index
-
 wget https://cqsweb.app.vumc.org/download1/bowtie_index/mirBase21.tar.gz
 tar -xzvf mirBase21.tar.gz
 wget https://cqsweb.app.vumc.org/download1/bowtie_index/GtRNAdb2.tar.gz
@@ -23,24 +20,3 @@ tar -xzvf mm10.tar.gz
 wget https://cqsweb.app.vumc.org/download1/bowtie_index/rn5.tar.gz
 tar -xzvf rn5.tar.gz
 rm *.tar.gz
-
-cd ..
-mkdir annotation
-cd annotation
-
-wget https://cqsweb.app.vumc.org/download1/annotation/hg19_miRBase21_GtRNAdb2_gencode19_ncbi.bed
-wget https://cqsweb.app.vumc.org/download1/annotation/hg19_miRBase21_GtRNAdb2_gencode19_ncbi.bed.fa
-wget https://cqsweb.app.vumc.org/download1/annotation/hg19_miRBase21_GtRNAdb2_gencode19_ncbi.bed.info
-
-wget https://cqsweb.app.vumc.org/download1/annotation/hg38_miRBase21_GtRNAdb2_gencode25_ncbi.bed
-wget https://cqsweb.app.vumc.org/download1/annotation/hg38_miRBase21_GtRNAdb2_gencode25_ncbi.bed.fa
-wget https://cqsweb.app.vumc.org/download1/annotation/hg38_miRBase21_GtRNAdb2_gencode25_ncbi.bed.info
-
-wget https://cqsweb.app.vumc.org/download1/annotation/mm10_miRBase21_GtRNAdb2_gencode12_ncbi.bed
-wget https://cqsweb.app.vumc.org/download1/annotation/mm10_miRBase21_GtRNAdb2_gencode12_ncbi.bed.fa
-wget https://cqsweb.app.vumc.org/download1/annotation/mm10_miRBase21_GtRNAdb2_gencode12_ncbi.bed.info
-
-wget https://cqsweb.app.vumc.org/download1/annotation/rn5_miRBase21_GtRNAdb2_ensembl79_ncbi.bed
-wget https://cqsweb.app.vumc.org/download1/annotation/rn5_miRBase21_GtRNAdb2_ensembl79_ncbi.bed.fa
-wget https://cqsweb.app.vumc.org/download1/annotation/rn5_miRBase21_GtRNAdb2_ensembl79_ncbi.bed.info
-

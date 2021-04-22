d7e3e2f70eb7b49a710bf78a2381eefcbc34414c
Quanhu Sheng <shengqh@gmail.com>
2021-04-22 15:50:16 -0500

    v20210422

---
+++ tools/backup_database.sh
@@ -0,0 +1,4 @@
+cd /scratch/cqs_share/references/smallrna
+
+tar -czvf /workspace/shengq2/references/smallrna/20200305_viral_genomes.tar.gz 20200305_viral_genomes*
+tar -czvf /workspace/shengq2/references/smallrna/20200214_AlgaeSpeciesAll.tar.gz 20200214_AlgaeSpeciesAll*
